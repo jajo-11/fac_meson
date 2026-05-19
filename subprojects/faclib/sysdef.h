1296e4fc71b8acecbd00af87c0678f7ceea2f050
Joschka Goes <jgoes@mpi-hd.mpg.de>
2026-05-19 22:50:46 +0200

    add static sysdef.h for now

---
+++ subprojects/faclib/sysdef.h
@@ -0,0 +1,26 @@
+/* sysdef.h.  Generated from sysdef.h.in by configure.  */
+/* this is a template for sysdef.h */
+#ifndef _SYSDEF_H_ 
+#define _SYSDEF_H_ 1
+
+/* #undef f2cFortran */
+/* #undef NAGf90Fortran */
+/* #undef hpuxFortran */
+/* #undef apolloFortran */
+/* #undef sunFortran */
+/* #undef IBMR2Fortran */
+/* #undef CRAYFortran */
+/* #undef mipsFortran */
+/* #undef DECFortran */
+/* #undef vmsFortran */
+/* #undef CONVEXFortran */
+/* #undef PowerStationFortran */
+/* #undef AbsoftUNIXFortran */
+/* #undef SXFortran */
+/* #undef pgiFortran */
+/* #undef AbsoftProFortran */
+#define MOD_F2C 1
+#define PMALLOC 0
+#define USE_MPI 2
+
+#endif
